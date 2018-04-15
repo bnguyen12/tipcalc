68181cfbb92496189792dd673db91f84b8640f60
Benny Nguyen <benny.nguyenx@gmail.com>
2018-04-15 16:09:09 -0700

    Made layout

--- app/src/main/java/edu/washington/bennyn/tipcalc/MainActivity.kt
+++ app/src/main/java/edu/washington/bennyn/tipcalc/MainActivity.kt
@@ -2,11 +2,23 @@ package edu.washington.bennyn.tipcalc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputFilter
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var amount: EditText
+    lateinit var btn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn = findViewById(R.id.tipBtn)
+        btn.isEnabled = false
+
+        amount = findViewById(R.id.inputAmount)
     }
 }
