60e7b9b352a474d66c7f58be044d3643131c5925
Benny Nguyen <benny.nguyenx@gmail.com>
2018-04-15 18:39:02 -0700

    Made button enable when numbers are put in

--- app/src/main/java/edu/washington/bennyn/tipcalc/MainActivity.kt
+++ app/src/main/java/edu/washington/bennyn/tipcalc/MainActivity.kt
@@ -2,10 +2,12 @@ package edu.washington.bennyn.tipcalc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.text.InputFilter
+import android.text.Editable
 import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,10 +17,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         btn = findViewById(R.id.tipBtn)
         btn.isEnabled = false
-
         amount = findViewById(R.id.inputAmount)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        amount.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                btn.isEnabled = amount.text.isNotBlank()
+            }
+        })
     }
 }
